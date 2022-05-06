eeab407ca4df1ea66bce49971624271082a98218
nahteruya <nahteruya@gmail.com>
2022-05-06 15:58:13 -0300

    create script.ts file

---
+++ script.ts
@@ -0,0 +1,82 @@
+interface IVeiculo {
+  nome: string;
+  placa: string;
+  entrada: Date | string ;
+}
+
+const cadastrarBtn = document.getElementById('cadastrarBtn') as HTMLButtonElement;
+cadastrarBtn.addEventListener('click', capturarDados);
+
+function capturarDados() {
+  const inputNomeVeiculo = document.getElementById('nome') as HTMLInputElement;
+  const nomeVeiculo = inputNomeVeiculo.value;
+
+  const inputPlacaVeiculo = document.getElementById('placa') as HTMLInputElement;
+  const placaVeiculo = inputPlacaVeiculo.value;
+
+  const novoVeiculo: IVeiculo = {
+    nome: nomeVeiculo,
+    placa: placaVeiculo,
+    entrada: new Date().toISOString()
+  }; 
+
+  if(!nomeVeiculo || !placaVeiculo) {
+    alert("Os campos nome e placa do veículo são obrigatórios!");
+    return;
+  } 
+  manipularPatio().adicionarVeiculo(novoVeiculo, true);
+}
+
+manipularPatio().renderDadosLocalStorage();
+
+function manipularPatio() {
+  function adicionarVeiculo (veiculo: IVeiculo, salvo?: boolean) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${veiculo.nome}</tr>
+      <td>${veiculo.placa}</tr>
+      <td>${veiculo.entrada}</tr>
+      <td>
+        <button class='delete' data-placa='${veiculo.placa}'>Excluir</button>
+      </tr>`;
+    document.getElementById('patio')?.append(row);
+
+    const excluirBtn = row.querySelector('.delete') as HTMLButtonElement;
+    excluirBtn.addEventListener('click', function() {
+      removerVeiculo(this.dataset.placa);
+    })
+
+    if (salvo) salvarDadosLocalStorage([...lerDadosLocalStorage(), veiculo]);
+  }
+  function lerDadosLocalStorage(): IVeiculo[] {
+    return localStorage.patio ? JSON.parse(localStorage.patio) : [];
+  }
+  function salvarDadosLocalStorage(veiculos: IVeiculo[]) {
+    localStorage.setItem('patio', JSON.stringify(veiculos));
+  }
+  function renderDadosLocalStorage() {
+    document.getElementById('patio')!.innerHTML = '';
+    const patio = lerDadosLocalStorage();
+    if (patio.length) {
+      patio.forEach(veiculo => adicionarVeiculo(veiculo));
+    }
+  }
+  function removerVeiculo(placa:string) {
+    const veiculoParaRemover = lerDadosLocalStorage().find((veiculo:IVeiculo) => veiculo.placa == placa);
+
+    const tempoNoPatio = calcularTempo((new Date).getTime() - new Date((veiculoParaRemover.entrada)).getTime());
+
+    if (!confirm(`O veículo permaneceu por ${tempoNoPatio}. Deseja encerrar?`)) return;
+
+    salvarDadosLocalStorage(lerDadosLocalStorage().filter((veiculo) => veiculo.placa !== placa));
+    renderDadosLocalStorage();
+  }
+  return {adicionarVeiculo, lerDadosLocalStorage, salvarDadosLocalStorage, renderDadosLocalStorage, removerVeiculo};
+}
+
+function calcularTempo(miliseg:number) {
+  const hora = Math.floor(miliseg/3600000);
+  const min = Math.floor((miliseg%3600000)/60000);
+
+  return `${hora} horas e ${min} minutos`;
+}
